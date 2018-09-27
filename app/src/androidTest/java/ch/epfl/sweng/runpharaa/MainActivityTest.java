65c7c6e4fd2a3e13bc7ab1afc695c74831e320b7
somecookie <ricardo.ferreiraribeiro@epfl.ch>
2018-09-27 17:31:07 +0200

    Remvoe the .idea/ of the git

---
+++ app/src/androidTest/java/ch/epfl/sweng/runpharaa/MainActivityTest.java
@@ -0,0 +1,21 @@
+package ch.epfl.sweng.runpharaa;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void testToComplete() {
+        //test
+    }
+}
