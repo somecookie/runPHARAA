0052aded8f404bf9f38365b07a06f8b5fcf8a1f1
Anton Ragot <anton.ragot@epfl.ch>
2018-09-27 15:51:52 +0200

    Adding fields of user

---
+++ app/src/main/java/ch/epfl/sweng/runpharaa/User.java
@@ -0,0 +1,16 @@
+package ch.epfl.sweng.runpharaa;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class User {
+
+    private static String name;
+    private static File picture;
+    private static ArrayList<Track> list_of_created_tracks;
+    private static ArrayList<Track> list_of_pref;
+    //TODO: set localisation champ
+    private static Boolean admin;
+    private static int uId;
+
+}
--- app/src/main/java/ch/epfl/sweng/runpharaa/user.java
+++
@@ -1,4 +0,0 @@
-package ch.epfl.sweng.runpharaa;
-
-public class user {
-}
