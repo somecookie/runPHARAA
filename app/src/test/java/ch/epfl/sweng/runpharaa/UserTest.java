85d207704e60278fc154eb0c37ae72183bd4eba5
Anton Ragot <anton.ragot@epfl.ch>
2018-09-28 10:49:44 +0200

    Adding a few tests

--- app/src/test/java/ch/epfl/sweng/runpharaa/UserTest.java
+++ app/src/test/java/ch/epfl/sweng/runpharaa/UserTest.java
@@ -16,10 +16,37 @@ public class UserTest {
 
         assertEquals(guest.getName(), "test");
         //TODO: Tester l'image une fois que l'implementation est faite
+        assertEquals(guest.getPicture(), null);
         assertEquals(guest.getList_of_created_tracks(), empty_list_of_tracks);
         assertEquals(guest.getList_of_pref(), empty_list_of_tracks);
         //TODO: Tester avec des vraies tracks une fois que @Hugo aura fini l'implementation
         assertEquals(guest.isAdmin(), false);
         assertEquals(guest.getuId(), 1);
+    }
+
+    @Test
+    public void setterTest(){
+        ArrayList empty_list_of_tracks = new ArrayList<>();
+
+        User guest = new User("test", null, empty_list_of_tracks, empty_list_of_tracks, false, 1);
+
+        guest.setName("newName");
+        //TODO: See for testing pictures
+        guest.setPicture(null);
+        //TODO: change once we can create tracks
+        guest.setList_of_created_tracks(new ArrayList<Track>());
+        guest.setList_of_pref(new ArrayList<Track>());
+        guest.setAdmin(true);
+        guest.setuId(3);
+
+
+        assertEquals(guest.getName(), "newName");
+        //TODO: Tester l'image une fois que l'implementation est faite
+        assertEquals(guest.getPicture(), null);
+        assertEquals(guest.getList_of_created_tracks(), new ArrayList<Track>());
+        assertEquals(guest.getList_of_pref(), new ArrayList<Track>());
+        //TODO: Tester avec des vraies tracks une fois que @Hugo aura fini l'implementation
+        assertEquals(guest.isAdmin(), true);
+        assertEquals(guest.getuId(), 3);
     }
 }
