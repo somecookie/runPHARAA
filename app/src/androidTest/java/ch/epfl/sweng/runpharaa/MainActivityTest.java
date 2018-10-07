9a66e086fccb925d681dc19f3a6e36a2e12cc606
Anton Ragot <anton.ragot@epfl.ch>
2018-10-07 12:44:34 +0200

    Adding a swipe down action to refresh

--- app/src/androidTest/java/ch/epfl/sweng/runpharaa/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/runpharaa/MainActivityTest.java
@@ -19,6 +19,7 @@ import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
@@ -37,6 +38,7 @@ public class MainActivityTest {
 
     @Test
     public void testChangeFragment() {
+        onView(withId(R.id.tabLayoutId)).perform(swipeDown());
         onView(withId(R.id.tabLayoutId)).perform(swipeRight()).perform(swipeRight());
         }
 
