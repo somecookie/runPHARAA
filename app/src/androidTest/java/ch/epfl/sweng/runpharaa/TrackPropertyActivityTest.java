956bb291bb105c679147d6fc1e0ff1655cdd38de
Xael <alexandre.abbey@epfl.ch>
2018-10-15 19:55:17 +0200

    Travis test attempt

--- app/src/androidTest/java/ch/epfl/sweng/runpharaa/TrackPropertyActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/runpharaa/TrackPropertyActivityTest.java
@@ -5,19 +5,12 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.google.android.gms.maps.model.LatLng;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.swipeDown;
-import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
