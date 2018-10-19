eb9e3583f296463893db5602c8ecaa412485cbd6
Anton Ragot <anton.ragot@epfl.ch>
2018-10-19 09:50:16 +0200

    Adding Create track tests

--- app/src/androidTest/java/ch/epfl/sweng/runpharaa/CreateTrackActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/runpharaa/CreateTrackActivityTest.java
@@ -29,9 +29,12 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
 public class CreateTrackActivityTest {
@@ -51,40 +54,6 @@ public class CreateTrackActivityTest {
                 .getUiAutomation()
                 //.executeShellCommand(String.format("appops set %s android:mock_location allow", "ch.epfl.sweng.runpharaa"));
                 .executeShellCommand(String.format("appops set %s android:mock_location allow", mActivityRule.getActivity().getPackageName()));
-    }
-
-    @Test
-    public void createTrackWorksWithTwoFakePoints() {
-        LocationManager mLocationManager = (LocationManager)this.mActivityRule.getActivity().getSystemService(Context.LOCATION_SERVICE);;
-        /* every time you mock location, you should use these code */
-        int value = setMockLocationSettings();//toggle ALLOW_MOCK_LOCATION on
-        try {
-        System.out.print(mActivityRule.getActivity().getPackageName());
-        locMgr = (LocationManager) mActivityRule.getActivity().getSystemService(Context.LOCATION_SERVICE);
-        // Let map load
-        sleep(2000);
-        setMock(46.518577, 6.563165, 1);
-        sleep(500);
-        onView(withId(R.id.start_create_button)).perform(click());
-        sleep(500);
-        setMock(46.522735, 6.579772, 1);
-        sleep(500);
-        onView(withId(R.id.start_create_button)).perform(click());
-        onView(withId(R.id.create_text_total_altitude)).check(matches(withText("Total altitude difference: 0.00 m")));
-        onView(withId(R.id.create_text_name)).perform(typeText("Random name")).perform(closeSoftKeyboard());
-        onView(withId(R.id.set_properties)).perform(click());
-        onView(withId(R.id.time)).perform(typeText("10.00")).perform(closeSoftKeyboard());
-        sleep(1000);
-        onView(withId(R.id.types)).perform(click());
-        onView(withId(R.id.cardListId)).perform(click());
-        pressBack();
-        onView(withId(R.id.create_track_button)).perform(click());
-
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } finally {
-            restoreMockLocationSettings(value);//toggle ALLOW_MOCK_LOCATION off
-        }
     }
 
     private int setMockLocationSettings() {
@@ -109,7 +78,7 @@ public class CreateTrackActivityTest {
         }
     }
 
-    
+
 
     private LocationManager locMgr;
 
@@ -143,4 +112,106 @@ public class CreateTrackActivityTest {
 
         locMgr.setTestProviderLocation(LocationManager.GPS_PROVIDER, newLocation);
     }
+
+    /*
+    @Test
+    public void createTrackWorksWithTwoFakePoints() {
+        int value = setMockLocationSettings();//toggle ALLOW_MOCK_LOCATION on
+        try {
+        System.out.print(mActivityRule.getActivity().getPackageName());
+        locMgr = (LocationManager) mActivityRule.getActivity().getSystemService(Context.LOCATION_SERVICE);
+        // Let map load
+        sleep(2000);
+        setMock(46.518577, 6.563165, 1);
+        sleep(500);
+        onView(withId(R.id.start_create_button)).perform(click());
+        sleep(500);
+        setMock(46.522735, 6.579772, 1);
+        sleep(500);
+        onView(withId(R.id.start_create_button)).perform(click());
+        onView(withId(R.id.create_text_total_altitude)).check(matches(withText("Total altitude difference: 0.00 m")));
+        onView(withId(R.id.create_text_name)).perform(typeText("Random name")).perform(closeSoftKeyboard());
+        onView(withId(R.id.set_properties)).perform(click());
+        onView(withId(R.id.time)).perform(typeText("10.00")).perform(closeSoftKeyboard());
+        sleep(1000);
+        onView(withId(R.id.types)).perform(click());
+        onView(withId(R.id.cardListId)).perform(click());
+        pressBack();
+        onView(withId(R.id.create_track_button)).perform(click());
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            restoreMockLocationSettings(value);//toggle ALLOW_MOCK_LOCATION off
+        }
+    }*/
+
+
+
+
+    @Test
+    public void needAtLeastTwoPointsToCreateTracks(){
+        int value = setMockLocationSettings();//toggle ALLOW_MOCK_LOCATION on
+        try {
+            System.out.print(mActivityRule.getActivity().getPackageName());
+            locMgr = (LocationManager) mActivityRule.getActivity().getSystemService(Context.LOCATION_SERVICE);
+            // Let map load
+            sleep(2000);
+            //Setting location to the Burj Kahlifa
+            setMock(25.197401, 55.274377, 1);
+            sleep(500);
+            onView(withId(R.id.start_create_button)).perform(click());
+            sleep(500);
+            onView(withId(R.id.start_create_button)).perform(click());
+            onView(withText("You need at least 2 points to create a track !"))
+                    .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                    .check(matches(isDisplayed()));
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            restoreMockLocationSettings(value);//toggle ALLOW_MOCK_LOCATION off
+        }
+    }
+
+
+    @Test
+    public void tryCreatingTrackWithoutSettingProperties(){
+        int value = setMockLocationSettings();//toggle ALLOW_MOCK_LOCATION on
+        try {
+            System.out.print(mActivityRule.getActivity().getPackageName());
+            locMgr = (LocationManager) mActivityRule.getActivity().getSystemService(Context.LOCATION_SERVICE);
+            // Let map load
+            sleep(2000);
+            //Setting location to Buckingham Palace
+            setMock(51.501478, -0.141702, 1);
+            sleep(500);
+            onView(withId(R.id.start_create_button)).perform(click());
+            sleep(500);
+            //To local pub
+            setMock(51.499248, -0.136834, 1);
+            sleep(500);
+            onView(withId(R.id.start_create_button)).perform(click());
+            onView(withId(R.id.create_text_name)).perform(typeText("From Buckingham palace to local pub")).perform(closeSoftKeyboard());
+            //We need to wait long enough for the "Ending GPS signal toast to disappear"
+            sleep(2000);
+            onView(withId(R.id.create_track_button)).perform(click());
+            onView(withText("The properties must be set up"))
+                    .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                    .check(matches(isDisplayed()));
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            restoreMockLocationSettings(value);//toggle ALLOW_MOCK_LOCATION off
+        }
+    }
+
+    /*@Test
+    public void tryCreatingTrackWithoutSettingName(){
+
+    }
+
+    @Test
+    public void tryCreatingTrackWithoutSettingTypes(){
+
+    }*/
 }
