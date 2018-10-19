1d56588679a6ebb269a94993796f7a62406cd9ea
Anton Ragot <anton.ragot@epfl.ch>
2018-10-19 10:53:52 +0200

    Adding more tests

--- app/src/androidTest/java/ch/epfl/sweng/runpharaa/CreateTrackActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/runpharaa/CreateTrackActivityTest.java
@@ -10,30 +10,52 @@ import android.os.SystemClock;
 import android.provider.Settings;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.SeekBar;
 
 import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.maps.model.LatLng;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.HashSet;
+
 import static android.os.SystemClock.sleep;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressKey;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.RootMatchers.isDialog;
 import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
@@ -47,6 +69,11 @@ public class CreateTrackActivityTest {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION);
 
+    @BeforeClass
+    public static void initUser(){
+        User.instance = new User("FakeUser", 2000, null, new HashSet<Integer>(), new HashSet<Integer>(), new LatLng(21.23, 12.112), false, "aa");
+    }
+
     @Before
     public void grantPermissions() {
         InstrumentationRegistry
@@ -55,6 +82,8 @@ public class CreateTrackActivityTest {
                 //.executeShellCommand(String.format("appops set %s android:mock_location allow", "ch.epfl.sweng.runpharaa"));
                 .executeShellCommand(String.format("appops set %s android:mock_location allow", mActivityRule.getActivity().getPackageName()));
     }
+
+    //------------- METHOD TO MOCK LOCATION, GRANT PERMISSION AND SEEKBAR ----------------
 
     private int setMockLocationSettings() {
         int value = 1;
@@ -113,7 +142,27 @@ public class CreateTrackActivityTest {
         locMgr.setTestProviderLocation(LocationManager.GPS_PROVIDER, newLocation);
     }
 
-    /*
+    private static ViewAction setProgress(final int progress) {
+        return new ViewAction() {
+            @Override
+            public void perform(UiController uiController, View view) {
+                SeekBar seekBar = (SeekBar) view;
+                seekBar.setProgress(progress);
+            }
+            @Override
+            public String getDescription() {
+                return "Set a progress on a SeekBar";
+            }
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(SeekBar.class);
+            }
+        };
+    }
+
+    //----------------------------TESTS-------------------------------
+
+
     @Test
     public void createTrackWorksWithTwoFakePoints() {
         int value = setMockLocationSettings();//toggle ALLOW_MOCK_LOCATION on
@@ -132,11 +181,22 @@ public class CreateTrackActivityTest {
         onView(withId(R.id.create_text_total_altitude)).check(matches(withText("Total altitude difference: 0.00 m")));
         onView(withId(R.id.create_text_name)).perform(typeText("Random name")).perform(closeSoftKeyboard());
         onView(withId(R.id.set_properties)).perform(click());
-        onView(withId(R.id.time)).perform(typeText("10.00")).perform(closeSoftKeyboard());
+        onView(withId(R.id.time)).perform(typeText("10.00"))
+                .perform(pressKey(KeyEvent.KEYCODE_ENTER))
+                .perform(closeSoftKeyboard());
+        onView(withText("OK"))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()))
+                .perform(click());
         sleep(1000);
         onView(withId(R.id.types)).perform(click());
-        onView(withId(R.id.cardListId)).perform(click());
-        pressBack();
+        onData(is(instanceOf(String.class))).inAdapterView(allOf(withClassName(equalTo("com.android.internal.app.AlertController$RecycleListView")), isDisplayed()))
+                .atPosition(0)
+                .perform(click());
+        onView(withText("OK"))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()))
+                .perform(click());
         onView(withId(R.id.create_track_button)).perform(click());
 
         } catch (SecurityException e) {
@@ -144,7 +204,7 @@ public class CreateTrackActivityTest {
         } finally {
             restoreMockLocationSettings(value);//toggle ALLOW_MOCK_LOCATION off
         }
-    }*/
+    }
 
 
 
@@ -205,13 +265,93 @@ public class CreateTrackActivityTest {
         }
     }
 
-    /*@Test
-    public void tryCreatingTrackWithoutSettingName(){
-
-    }
 
     @Test
     public void tryCreatingTrackWithoutSettingTypes(){
+    int value = setMockLocationSettings();//toggle ALLOW_MOCK_LOCATION on
+        try {
+            System.out.print(mActivityRule.getActivity().getPackageName());
+            locMgr = (LocationManager) mActivityRule.getActivity().getSystemService(Context.LOCATION_SERVICE);
+            // Let map load
+            sleep(2000);
+            //Marina Bay Sands
+            setMock(1.283536, 103.860319, 1);
+            sleep(500);
+            onView(withId(R.id.start_create_button)).perform(click());
+            sleep(500);
+            //To Esplanade theater
+            setMock(1.288845, 103.855491, 1);
+            sleep(500);
+            onView(withId(R.id.start_create_button)).perform(click());
+            onView(withId(R.id.create_text_name)).perform(typeText("Small walk near Marina Bay")).perform(closeSoftKeyboard());
+            onView(withId(R.id.set_properties)).perform(click());
+            sleep(1000);
+            onView(withText("OK"))
+                    .inRoot(isDialog())
+                    .check(matches(isDisplayed()))
+                    .perform(click());
+            sleep(3000);
+            onView(withId(R.id.create_track_button)).perform(click());
+            onView(withText("The types must be set up"))
+                    .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                    .check(matches(isDisplayed()));
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            restoreMockLocationSettings(value);//toggle ALLOW_MOCK_LOCATION off
+        }
+    }
 
-    }*/
+    @Test
+    public void tryCreatingTrackWithoutSettingName() {
+        int value = setMockLocationSettings();//toggle ALLOW_MOCK_LOCATION on
+        try {
+            System.out.print(mActivityRule.getActivity().getPackageName());
+            locMgr = (LocationManager) mActivityRule.getActivity().getSystemService(Context.LOCATION_SERVICE);
+            // Let map load
+            sleep(2000);
+            //Eiffel Tower
+            setMock(48.858664, 2.294424, 1);
+            sleep(500);
+            onView(withId(R.id.start_create_button)).perform(click());
+            sleep(500);
+            //To place du trocadero
+            setMock(48.863048, 2.287890, 1);
+            sleep(500);
+            onView(withId(R.id.start_create_button)).perform(click());
+            onView(withId(R.id.create_text_total_altitude)).check(matches(withText("Total altitude difference: 0.00 m")));
+            onView(withId(R.id.set_properties)).perform(click());
+            onView(withClassName(Matchers.equalTo(SeekBar.class.getName()))).perform(setProgress(1));
+            onView(withId(R.id.diff_text)).check(matches(withText("Difficulty is 1")));
+            sleep(1000);
+            onView(withText("OK"))
+                    .inRoot(isDialog())
+                    .check(matches(isDisplayed()))
+                    .perform(click());
+            sleep(3000);
+            onView(withText("Default run time was chosen"))
+                    .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                    .check(matches(isDisplayed()));
+            sleep(3000);
+            onView(withId(R.id.types)).perform(click());
+            onData(is(instanceOf(String.class))).inAdapterView(allOf(withClassName(equalTo("com.android.internal.app.AlertController$RecycleListView")), isDisplayed()))
+                    .atPosition(0)
+                    .perform(click());
+            onView(withText("OK"))
+                    .inRoot(isDialog())
+                    .check(matches(isDisplayed()))
+                    .perform(click());
+            onView(withId(R.id.create_track_button)).perform(click());
+            onView(withText("A track needs a name!"))
+                    .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                    .check(matches(isDisplayed()));
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            restoreMockLocationSettings(value);//toggle ALLOW_MOCK_LOCATION off
+        }
+    }
+
+
+
 }
