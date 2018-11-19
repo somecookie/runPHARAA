4cfda4b707440d6896666e3574d3c24c77fbded1
Hugo Decroix <hugo.decroix@epfl.ch>
2018-11-19 12:54:38 +0100

    Ignore failing test and add more test

--- app/src/androidTest/java/ch/epfl/sweng/runpharaa/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/runpharaa/MainActivityTest.java
@@ -216,11 +216,23 @@ public class MainActivityTest extends TestInitLocation {
         int length = 100;
         int heigthDiff = 10;
         FirebaseTrackAdapter track = new FirebaseTrackAdapter("Cours forest !", "0", "BobUID", "Bob", Arrays.asList(coord0, coord1), "imageUri",
-                types, length, heigthDiff, 1, 1, 1, 1, 0, 0);
+                types, length, heigthDiff, 2, 1, 40, 1, 0, 0);
         Track t = new Track(track);
         assertTrue(MainActivity.passFilters(t));
         MainActivity.typesFilter.remove(TrackType.FOREST);
         MainActivity.typesFilter.add(TrackType.CITY);
+        assertFalse(MainActivity.passFilters(t));
+        MainActivity.typesAreFiltered = false;
+        assertTrue(MainActivity.passFilters(t));
+        MainActivity.timeIsFiltered = false;
+        assertTrue(MainActivity.passFilters(t));
+        MainActivity.difficultyIsFiltered = true;
+        MainActivity.typesAreFiltered = true;
+        assertFalse(MainActivity.passFilters(t));
+        MainActivity.typesAreFiltered = false;
+        assertTrue(MainActivity.passFilters(t));
+        MainActivity.difficultyIsFiltered = false;
+        MainActivity.typesAreFiltered = true;
         assertFalse(MainActivity.passFilters(t));
     }
 
