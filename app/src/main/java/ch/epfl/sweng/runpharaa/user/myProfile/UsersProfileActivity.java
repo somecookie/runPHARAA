c5bdecaaa4d8a270b1987b488a048145e47ca292
Hugo Decroix <hugo.decroix@epfl.ch>
2018-11-27 16:23:01 +0100

    Setting icon and signout icon in useracticity action bar

--- app/src/main/java/ch/epfl/sweng/runpharaa/user/myProfile/UsersProfileActivity.java
+++ app/src/main/java/ch/epfl/sweng/runpharaa/user/myProfile/UsersProfileActivity.java
@@ -3,15 +3,20 @@ package ch.epfl.sweng.runpharaa.user.myProfile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -62,6 +67,13 @@ public class UsersProfileActivity extends AppCompatActivity {
 
         // Remove shadow from action bar
         getSupportActionBar().setElevation(0);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_menu_user, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -73,6 +85,20 @@ public class UsersProfileActivity extends AppCompatActivity {
     protected void setEmptyMessage() {
         emptyMessage.setText(R.string.no_created_self);
         emptyMessage.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settingsIcon :
+                startActivity(new Intent(getBaseContext(), SettingsActivity.class));
+                return true;
+            case R.id.signOutIcon :
+                signOut();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void loadActivity(User user) {
@@ -89,12 +115,6 @@ public class UsersProfileActivity extends AppCompatActivity {
         TextView v2 = findViewById(R.id.nbFav);
         int nbFav = user.getFavoriteTracks().size();
         v2.setText(Integer.toString(nbFav));
-
-        ImageButton settingsButton = findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(v3 -> startActivity(new Intent(getBaseContext(), SettingsActivity.class)));
-
-        Button signOutButton = findViewById(R.id.sign_out_button);
-        signOutButton.setOnClickListener(v32 -> signOut());
 
         picture = findViewById(R.id.profile_picture);
         ImageLoader.getLoader(this).displayImage(User.instance.getPicture(), picture);
