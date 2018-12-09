9fba7a4254b893946e8290f4fb7b20c44e8e32e5
Anton Ragot <anton.ragot@epfl.ch>
2018-12-09 22:48:39 +0100

    Notification working perfectly

--- app/src/main/java/ch/epfl/sweng/runpharaa/notification/FirebaseNotification.java
+++ app/src/main/java/ch/epfl/sweng/runpharaa/notification/FirebaseNotification.java
@@ -1,19 +1,25 @@
 package ch.epfl.sweng.runpharaa.notification;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 
+import java.util.Date;
+
 import ch.epfl.sweng.runpharaa.MainActivity;
 import ch.epfl.sweng.runpharaa.R;
 import ch.epfl.sweng.runpharaa.database.UserDatabaseManagement;
+
 
 /**
  * Class that handle the received notification
@@ -39,15 +45,29 @@ public class FirebaseNotification extends FirebaseMessagingService {
                 NotificationCompat.Builder(this, "notification")
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(remoteMessage.getData().get("title"))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(remoteMessage.getData().get("message")))
                 .setContentText(remoteMessage.getData().get("message"))
                 .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_HIGH)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
                 .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager)
                         getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(idRequest, notificationBuilder.build());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "1";
+            @SuppressLint("WrongConstant") NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_MAX);
+            notificationManager.createNotificationChannel(channel);
+            notificationBuilder.setChannelId(channelId);
+        }
+
+        //The first parameter is a random number to allow to have multiple different notification
+        //Put a fixed number to allow only one notification
+        notificationManager.notify((int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE), notificationBuilder.build());
     }
 
 
